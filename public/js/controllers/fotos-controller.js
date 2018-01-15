7b5e8700c92f9315ebf27b72cae3c0c08c9333a1
Pauloalgelin <pauloalgelin@gmail.com>
2018-01-15 10:56:40 -0200

    01 Construindo o alicerce da aplicação

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', FotosController);
+function FotosController ($scope){
+  $scope.foto = {
+    titulo: 'Sapato',
+    url: 'https://m.media-amazon.com/images/I/81qj31vwQwL._SX480_.jpg'
+  };
+}
